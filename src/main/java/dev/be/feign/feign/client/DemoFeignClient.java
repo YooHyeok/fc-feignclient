04d5511846f1faa4ac0747c60c848060b3275767
webdevyoo <yjk7454@naver.com>
2024-04-11 09:36:11 +0900

    feign property(yaml) 세팅 및 FeignClient(Class) 구현

---
+++ src/main/java/dev/be/feign/feign/client/DemoFeignClient.java
@@ -0,0 +1,22 @@
+package dev.be.feign.feign.client;
+
+import dev.be.feign.common.dto.BaseRequestInfo;
+import dev.be.feign.common.dto.BaseResponseInfo;
+import dev.be.feign.feign.config.DemoFeignConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        name = "demo-client", /* 해당 인터페이스를 지칭하는 유일한 PK 역할을 해주는 값 */
+        url = "${feign.url.prefix}", /* 현재 Client가 요청을 보내고자 하는 Target이 되는 Server의 URL 값 */
+        configuration = DemoFeignConfig.class
+)
+public interface DemoFeignClient {
+
+    @GetMapping("/get") // @FeignClient의 url에 지정한 application.yaml파일의 prefix 주소값이 앞에 붙게됨 - http://localhost:8080/target-server/get
+    ResponseEntity<BaseResponseInfo> callGet(@RequestHeader("CustomHeaderName") String customHeader,
+                                             @RequestParam("name") String name, @RequestParam("age") Long age);
+}
