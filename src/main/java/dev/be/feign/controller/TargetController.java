62f0e8a88ae1e7b0b86e525df9ba4602bc2f958f
webdevyoo <yjk7454@naver.com>
2024-04-13 00:27:53 +0900

    ErrorDecoder - `/error` 외부 API 추가

--- src/main/java/dev/be/feign/controller/TargetController.java
+++ src/main/java/dev/be/feign/controller/TargetController.java
@@ -2,6 +2,8 @@ package dev.be.feign.controller;
 
 import dev.be.feign.common.dto.BaseRequestInfo;
 import dev.be.feign.common.dto.BaseResponseInfo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,5 +26,9 @@ public class TargetController {
                 .name(body.getName())
                 .age(body.getAge())
                 .build();
+    }
+    @GetMapping("/error")
+    public ResponseEntity<BaseResponseInfo> demoError() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
