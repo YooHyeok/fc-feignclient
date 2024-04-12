8e14615e0a903199d7cc3fce325a37da58ed9ce2
webdevyoo <yjk7454@naver.com>
2024-04-12 11:37:42 +0900

    외부 api 호출을 위한 Controller/Service 구현 - feignclient post 호출

--- src/main/java/dev/be/feign/service/DemoService.java
+++ src/main/java/dev/be/feign/service/DemoService.java
@@ -1,5 +1,6 @@
 package dev.be.feign.service;
 
+import dev.be.feign.common.dto.BaseRequestInfo;
 import dev.be.feign.common.dto.BaseResponseInfo;
 import dev.be.feign.feign.client.DemoFeignClient;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,18 @@ public class DemoService {
     private final DemoFeignClient demoFeignClient;
     public String get() {
         ResponseEntity<BaseResponseInfo> response = demoFeignClient.callGet("CustomHeader", "CustomName", 1L);
+        System.out.println("Name: " + response.getBody().getName());
+        System.out.println("Age: " + response.getBody().getAge());
+        System.out.println("Header: " + response.getBody().getHeader());
+        return "get";
+    }
+
+    public String post() {
+        BaseRequestInfo baseRequestInfo = BaseRequestInfo.builder()
+                .name("CustomName")
+                .age(2L)
+                .build();
+        ResponseEntity<BaseResponseInfo> response = demoFeignClient.callPost("CustomHeader", baseRequestInfo);
         System.out.println("Name: " + response.getBody().getName());
         System.out.println("Age: " + response.getBody().getAge());
         System.out.println("Header: " + response.getBody().getHeader());
